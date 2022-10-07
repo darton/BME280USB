9d36fbc3e75a28db3bfc2156a99b3d94cc6c0dc5
Dariusz Kowalczyk <darton@users.noreply.github.com>
2022-10-07 16:21:25 +0200

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -5,8 +5,9 @@ from time import sleep
 
 root = Tk()
 root.title('BME280')
+root.geometry("400x50")
 var = StringVar()
-lbl = Label(root, textvariable = var , width=40, height=5, font=('Consolas', 24, 'bold'), fg="blue")
+lbl = Label(root, textvariable = var, font=('Consolas', 24, 'bold'), fg="blue")
 lbl.pack()
 
 serialPort = serial.Serial(port="COM6", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
