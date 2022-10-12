6e2339f0f2baae95a6fb1333450f75eecb315f1b
Dariusz Kowalczyk <darton@users.noreply.github.com>
2022-10-12 15:45:52 +0200

    Update and rename sensor.py to bme280usb_gui.py

--- bme280usb_gui.py
+++ bme280usb_gui.py
@@ -2,6 +2,8 @@ from tkinter import Tk, StringVar, Label
 import threading
 import serial
 from time import sleep
+import platform
+import sys
 
 root = Tk()
 root.title('BME280')
@@ -10,7 +12,16 @@ var = StringVar()
 lbl = Label(root, textvariable = var, font=('Consolas', 24, 'bold'), fg="blue")
 lbl.pack()
 
-serialPort = serial.Serial(port="COM6", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+_os = platform.system()
+
+if _os == 'Linux':
+    serialPort = serial.Serial(port="/dev/serial/by-id/usb-MicroPython_Board_in_FS_mode_e66038b713882033-if00", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+elif _os == 'Windows':
+    serialPort = serial.Serial(port="COM6", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+else:
+    print('Unknown operating system')
+    sys.exit()
+
 serialString = ""
 
 while True:
