2a6332b81bebcc2d37ebab459dcc9ec5b4282f8d
Dariusz Kowalczyk <darton@users.noreply.github.com>
2022-10-13 20:52:10 +0200

    macOS support was added

--- bme280usb_gui.py
+++ bme280usb_gui.py
@@ -4,11 +4,14 @@ from time import sleep
 import platform
 import sys
 
+TK_SILENCE_DEPRECATION=1
+
 root = Tk()
 root.title('BME280')
 root.geometry("400x50")
 var = StringVar()
-lbl = Label(root, textvariable = var, font=('Consolas', 24, 'bold'), fg="blue")
+lbl = Label(root, textvariable = var, font=('Consolas', 24, 'bold'), 
+fg="blue")
 lbl.pack()
 
 _os = platform.system()
@@ -25,6 +28,12 @@ if _os == 'Linux':
 elif _os == 'Windows':
     try:
         serialPort = serial.Serial(port="COM6", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+    except:
+        print(error_msg)
+        sys.exit()
+elif _os == 'Darwin':
+    try:
+        serialPort = serial.Serial(port="/dev/cu.usbmodem11301", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
     except:
         print(error_msg)
         sys.exit()
@@ -45,6 +54,7 @@ while True:
                 t = round(int(a[1])/1000,1)
                 h = int(round(int(a[2])/1000,0))
                 p = int(round(int(a[3])/1000,0))
+                print(t, h, p)
                 var.set(f'{t}°C {h}% {p}hPa' )
                 root.update()
             except:
